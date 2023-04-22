10b1404fc5355b94d43e4beb313af364f4ad81c4
efirnet <efirnet@gmail.com>
2023-04-22 16:57:32 +0300

    add hw_01

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useCallback } from "react";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import RegistrationScreen from "./screens/RegistrationScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+export default function App() {
+  console.log(Platform.OS);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+          "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+          "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // width: "100%",
+    backgroundColor: "#000",
+    // alignItems: "center",
+    justifyContent: "center",
+    // borderWidth: 10,
+    // borderColor: "tomato",
+  },
+  text: {
+    fontFamily: "Roboto-Medium",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+});
